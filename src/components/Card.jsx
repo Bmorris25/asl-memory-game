9da886c8976d401d03efa0a60fc7f140c2d81868
Bmorris25 <brittany2morris@gmail.com>
2025-02-19 10:04:46 -0800

    compress images and add lazy loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,26 @@
-import React,{useEffect} from "react";
+import React, { useEffect, useRef } from "react";
 
 const Card = ({ card, handleClick, isFlipped }) => {
+  const audioRef = useRef(null); // Store audio reference
+
   useEffect(() => {
     if (isFlipped && card.type === "word" && card.audio) {
-      // Create an audio object and play it
+      if (audioRef.current) {
+        audioRef.current.pause(); // Stop previous audio if playing
+        audioRef.current.currentTime = 0; // Reset playback position
+      }
+
       const audio = new Audio(card.audio);
-      audio.play();
+      audioRef.current = audio;
+      audio.play().catch((err) => console.error("Audio play failed", err));
     }
   }, [isFlipped, card]);
+
   return (
     <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={() => handleClick(card)}>
       <div className={`card-inner ${isFlipped ? "flipped" : ""}`}>
         <div className="card-front">
-          <img className="img-fluid img-thumbnail" src={card.image} alt={card.name} />
+          <img className="img-fluid img-thumbnail" loading="lazy" src={card.image} alt={card.name} />
         </div>
         <div className="card-back"></div>
       </div>
@@ -21,3 +29,4 @@ const Card = ({ card, handleClick, isFlipped }) => {
 };
 
 export default Card;
+
